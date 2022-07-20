e54b52dc744e0bd0aa195f4ceac0408b1623fa60
Victor Calebe <victorcalebesilva10@gmail.com>
2022-07-20 11:29:25 -0300

    Desenvolvendo escolha de palavras e categorias de forma aleatória

--- src/App.js
+++ src/App.js
@@ -23,13 +23,57 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList)
 
-  console.log(words)
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(wordsList);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category);
+
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
+
+    return {word, category}
+  }
+
+//start secretWord game
+  const startGame = () => {
+    // pick word and pick category
+    const {word, category} = pickedWordAndCategory();
+    console.log(word, category);
+
+    // create an array of letters
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => (l.toLowerCase()));
+    console.log(wordLetters);
+
+    //fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
+    setGameStage(stages[1].name);
+  }
+
+//process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+//Restart the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className="App">
-      {gameStage === 'start' && <StartScreen/>}
-      {gameStage === 'game' && <Game/>}
-      {gameStage === 'end' && <GameOver/>}
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import './StartScreen.css'
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className="start">
         <h1>SecretWord</h1>
         <p>Clique aqui para começar a jogar!</p>
-        <button>Começar jogo</button>
+        <button onClick={startGame}>Começar jogo</button>
     </div>
   )
 }
