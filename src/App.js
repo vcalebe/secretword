1bf2b6dce3477583b0fa477acefde4a940f42b9a
Victor Calebe <victorcalebesilva10@gmail.com>
2022-07-20 10:05:43 -0300

    Adicionando componentes do projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+// CSS
 import './App.css';
 
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// data
+import { wordsList } from "./data/words";
+
+// components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+// array para gerenciar os stages do game
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"}
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className="App">
-      <h2>SecretWord</h2>
+      {gameStage === 'start' && <StartScreen/>}
+      {gameStage === 'game' && <Game/>}
+      {gameStage === 'end' && <GameOver/>}
     </div>
   );
 }
---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>SecretWord</h1>
+        <p>Clique aqui para começar a jogar!</p>
+        <button>Começar jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
