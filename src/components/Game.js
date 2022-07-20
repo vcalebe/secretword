243cf1ed2aebe93f1e6d7f383ffa5120433ce6b3
Victor Calebe <victorcalebesilva10@gmail.com>
2022-07-20 12:00:34 -0300

    Estruturando e estilizando tela do game

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,8 +3,34 @@ import './Game.css'
 const Game = ({verifyLetter}) => {
   return (
     <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar Jogo 👍</button>
+      <p className='game'>
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className='tip'>
+        Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+
+      <div className='wordContainer'>
+        <span className='letter'>A</span>
+        <span className='letter'></span>
+        <span className="blankSquare"></span>
+      </div>
+
+      <div className='letterContainer'>
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+
+      <div className="wrongLettersContainer">
+        <p>Letras Já utilizadas:</p>
+        <span>a, </span>
+        <span>b, </span>
+        <span>j, </span>
+      </div>
     </div>
   )
 }
